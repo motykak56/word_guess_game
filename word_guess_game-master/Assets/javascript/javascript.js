07dc57aa7176cf8e98856a96d51c884ef44bcd85
Kathleen Motyka <motykak@outlook.com>
2019-06-10 01:56:20 -0500

    to continue
    the editor I was using kept giving me an error message on my computer and freezing up.  It also kept losing my files and finally I moved to another editor.  That's when I posted to this repo by way of that editor and then git push wouldn't work on the old one.

---
+++ word_guess_game-master/Assets/javascript/javascript.js
@@ -0,0 +1,14 @@
+document 
+
+
+        document.body.style.background = 
+        $Image = "Assets/images/musicwallpaper.jpg";
+            
+        Write = Host-NoNewLine ('Press any key to continue...');
+        $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');       
+         
+         $answers = array("octave", "repeat", "bass", "eighth", "key", "time", "treble", "quarter", "quarter", "whole");
+         $theWord = "index ($answers)"
+         function uploadwordltr () 
+         {explode ("$answers",$theWord);}
+         print_r($answers);
---
+++ word_guess_game-master/Assets/javascript/write.js
@@ -0,0 +1,21 @@
+document.write("Text written using JavaScript code!");
+        document.
+        $Image = "Assets/images/musicwallpaper.jpg";
+            
+        Write = Host-NoNewLine ('Press any key to continue...');
+        $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');       
+         
+         $answers = array("octave", "repeat", "bass", "eighth", "key", "time", "treble", "quarter", "quarter", "whole");
+         $theWord = "index ($answers)"
+         function uploadwordltr () 
+         {explode ("$answers",$theWord);}
+         print_r($answers);
+
+         document.getElementById(question1); 
+         <button onclick="togglequestion()" id="keystroke">Show question</button> 
+         <function "togglequestion" ()
+         {
+             //get the question
+             var the_question = document.getElementById('question1');
+         }
+
